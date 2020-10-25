e19467063072419d50221b92170f823d35103929
Nazrin <nazrin.jafarli.std@bhos.edu.az>
2020-10-25 14:17:47 +0400

    add asignment2 file of week3

---
+++ WEEK3/assignment2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void swapStrings(char **destination,char **source);
+int main()
+{
+	char *str1="joker";
+	char *str2="cool";
+
+	
+	printf("str1:%s,str:%s\n",str1,str2);
+	swapStrings(&str1,&str2);
+	printf("str1:%s,str2:%s\n",str1,str2);
+	
+        return 0;
+}
+
+void swapStrings(char **destination,char **source)
+{
+		char *temp=*destination;
+		*destination=*source;
+		*source=temp;
+
+}
